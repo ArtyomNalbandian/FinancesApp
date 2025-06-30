cd733c3ac2a9e9a767f7687dcd930ffb77a6486a
ArtyomNalbandian <artyomnalbandian11a@mail.ru>
2025-07-01 00:57:16 +0300

    minor changes in main

--- app/src/main/java/com/example/financesapp/presentation/history/history_expenses/ExpensesHistoryIntent.kt
+++
@@ -1,9 +0,0 @@
-package com.example.financesapp.presentation.history.history_expenses
-
-sealed interface ExpensesHistoryIntent {
-
-    data class LoadHistory(
-        val startDate: String,
-        val endDate: String
-    ) : ExpensesHistoryIntent
-}
--- app/src/main/java/com/example/financesapp/presentation/history/history_income/IncomeHistoryIntent.kt
+++
@@ -1,9 +0,0 @@
-package com.example.financesapp.presentation.history.history_income
-
-sealed interface IncomeHistoryIntent {
-
-    data class LoadHistory(
-        val startDate: String,
-        val endDate: String
-    ) : IncomeHistoryIntent
-}
