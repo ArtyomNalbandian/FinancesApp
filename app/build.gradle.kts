76d74c034fd521580071decdeee860538165f2d6
ArtyomNalbandian <artyomnalbandian11a@mail.ru>
2025-07-26 06:17:51 +0300

    system splash implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.process)
 
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
